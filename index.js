763dec5fd02cdca2de694a39c50135bbb8dd69ab
yahyacodes <yahya.codin@gmail.com>
2023-09-15 17:55:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  let result = a + b;
+  return result;
+}
+function subtract(a, b) {
+  let result = a - b;
+  return result;
+}
+function multiply(a, b) {
+  let result = a * b;
+  return result;
+}
+function divide(a, b) {
+  let result = a / b;
+  return result;
+}
+
+function increment(n) {
+  let result = ++n;
+  return result;
+}
+
+function decrement(n) {
+  let result = --n;
+  return result;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
